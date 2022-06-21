5aced91c7c9f2ee399b6e24ba7ee4f92a9585be5
EmilieOLIVIE <emilie.olivie@hotmail.fr>
2022-06-21 15:17:34 +0100

    fix bug

--- src/services/acheterQtProduit.tsx
+++ src/services/acheterQtProduit.tsx
@@ -4,24 +4,6 @@ const ACHETER_QT_PRODUIT = gql`
 mutation acheterQtProduit($id: Int!, $quantite: Int!) {
   acheterQtProduit(id: $id, quantite: $quantite) {
     id
-    name
-    logo
-    cout
-    croissance
-    revenu
-    vitesse
-    quantite
-    timeleft
-    managerUnlocked
-    paliers {
-      name
-      logo
-      seuil
-      idcible
-      ratio
-      typeratio
-      unlocked
-    }
   }
 }
 `
